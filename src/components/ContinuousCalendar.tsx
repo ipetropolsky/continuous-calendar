bc9effae415d68d0d5860f9190c226e248d653df
Ivan Petropolsky <ivan.petropolsky@gmail.com>
2025-09-21 20:19:21 +0200

    master Fix month position

--- src/components/ContinuousCalendar.tsx
+++ src/components/ContinuousCalendar.tsx
@@ -638,7 +638,7 @@ export function ContinuousCalendar() {
                                                 handleMonthClick(firstDayOfMonth.year, firstDayOfMonth.monthName)
                                             }
                                         >
-                                            <div className="absolute p-1 z-20 right-full -translate-x-1 bottom-full origin-bottom-right -rotate-90 text-nowrap text-right text-xs font-serif font-medium text-gray-500 hover:text-blue-500 transition-colors inline-block cursor-pointer">
+                                            <div className="absolute p-1 z-20 right-full -translate-x-1 translate-y-1 bottom-full origin-bottom-right -rotate-90 text-nowrap text-right text-xs font-serif font-medium text-gray-500 hover:text-blue-500 transition-colors inline-block cursor-pointer">
                                                 {firstDayOfMonth.monthName}{' '}
                                                 <span className="text-gray-400">{firstDayOfMonth.year}</span>
                                             </div>
